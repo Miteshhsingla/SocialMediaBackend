41ae90dcb916913a0b18b87807e0cc04130335ef
miteshsingla <miteshhsingla@gmail.com>
2024-11-18 00:02:10 +0530

    fetch feed logic

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,16 +1,16 @@
 const pool = require('../config.js/db');
 
-
 //tested and working fine
 const createPost = async (req, res) => {
+    
     //extract image and content from req body;
     //fetch userId from req.user through middleware;
     //Insert the post using query;
     //return response to user on successful post created
+
   const { picture, content } = req.body;
   const userId = req.user.userId; // Assuming JWT middleware sets req.user
 
-  console.log('userId: ', userId);
   try {
     const newPost = await pool.query(
       'INSERT INTO posts (user_id, content, picture) VALUES ($1, $2, $3) RETURNING *',
@@ -23,5 +23,26 @@ const createPost = async (req, res) => {
   }
 };
 
+const getFeed = async (req,res) => {
+    //get userId
+    const userId = req.user.userId;
 
-module.exports = { createPost };
+    try {
+        const postsData = await pool.query(`SELECT posts.*,users.username FROM posts JOIN users ON
+            users.id = posts.user_id
+            WHERE posts.user_id IN
+            ( SELECT following_id FROM followers WHERE follower_id = $1 )
+            ORDER BY posts.created_at DESC`
+            ,[userId]);
+
+        if (postsData.rows.length === 0) {
+            res.send("Nothing to show");
+        } else {
+            res.status(200).json(postsData.rows);
+        }
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+};
+
+module.exports = { createPost, getFeed };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,12 +3,13 @@ const { registerUser, loginUser } = require('../controllers/auth.controller');
 const router = express.Router();
 const { followUser, unfollowUser } = require('../controllers/people.controller');
 const authMiddleware = require('../middlewares/authMiddleware');
-const { createPost } = require('../controllers/posts.controller');
+const { createPost, getFeed } = require('../controllers/posts.controller');
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 router.post('/addPost',authMiddleware,createPost);
 router.post('/follow', authMiddleware, followUser);
 router.delete('/unfollow/:unfollowId', authMiddleware, unfollowUser);
+router.get('/feed', authMiddleware, getFeed);
 
 module.exports = router;
